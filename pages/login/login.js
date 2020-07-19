f625202dd0e9fbfa19f24509e253994022cbd27f
hhq123 <846762142@qq.com>
2020-07-19 11:16:01 +0800

    Signed-off-by: hhq123 <846762142@qq.com>

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,126 @@
 // miniprogram/pages/login/login.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+      /**
+       * 页面的初始数据
+       */
+      data: {
 
-  },
+      },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
-  },
+      GetOpenId(){
+        var that = this;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+        wx.login({
+          success: function success(res){
+            var code = res.code;
+            //console.log(code)
+            wx.request({
+              url: 'url',
+              date:{
 
-  },
+              },
+              header:{
+                "Content-Type": "application/json"
+              },
+              method: 'Get',
+              dataType: 'json',
+              responseType: 'text',
+              success: function(res){
+                console.log(res.data);
+                that.globalData.Openid = res.data.ResponseData;
+                that.globalData.IsOpenid = res.data.DealResult;
+              },
+              fail: function(res){},
+              complete: function(res){},
+            })
+          }
+        })
+      },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+      bindGetUserInfo: function (res) {
+        if (res.detail.userInfo) {
+          //用户按了允许授权按钮
+          var that = this;
+          // 获取到用户的信息了，打印到控制台上看下
+          console.log("用户的信息如下：");
+          console.log(e.detail.userInfo);
+          //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
+          that.setData({
+            isHide: false
+          });
+        } else {
+          //用户按了拒绝按钮
+          wx.showModal({
+            title: '警告',
+            content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+            showCancel: false,
+            confirmText: '返回授权',
+            success: function (res) {
+              // 用户没有授权成功，不需要改变 isHide 的值
+              if (res.confirm) {
+                console.log('用户点击了“返回授权”');
+              }
+            }
+          });
+        }
+      },
 
-  },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+          /**
+           * 生命周期函数--监听页面加载
+           */
+          onLoad: function (options) {
 
-  },
+            },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+            /**
+             * 生命周期函数--监听页面初次渲染完成
+             */
+            onReady: function () {
 
-  },
+            },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+            /**
+             * 生命周期函数--监听页面显示
+             */
+            onShow: function () {
 
-  },
+            },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+            /**
+             * 生命周期函数--监听页面隐藏
+             */
+            onHide: function () {
 
-  },
+            },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+            /**
+             * 生命周期函数--监听页面卸载
+             */
+            onUnload: function () {
 
-  }
-})
+            },
+
+            /**
+             * 页面相关事件处理函数--监听用户下拉动作
+             */
+            onPullDownRefresh: function () {
+
+            },
+
+            /**
+             * 页面上拉触底事件的处理函数
+             */
+            onReachBottom: function () {
+
+            },
+
+            /**
+             * 用户点击右上角分享
+             */
+            onShareAppMessage: function () {
+
+            }
+        })
